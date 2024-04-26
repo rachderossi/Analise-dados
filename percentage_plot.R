5d3bfd09f26b060a0d9b16d7a0cd96aa32d8bc45
Raquel Rossi <raquelderossi@hotmail.com>
2024-04-26 16:32:33 +0100

    Update percentage_plot.R

--- percentage_plot.R
+++ percentage_plot.R
@@ -2,19 +2,19 @@ if (!require("readxl"))             install.packages("readxl")
 if (!require("plyr"))               install.packages("plyr")
 if (!require("ggplot2"))            install.packages("ggplot2")
 
-# frequency table
+# tabela de frequência
 table1::table1(~., data = dados)
 
-# first we need to group values to make our sample more representative (there were only a few responses for people over 41)
+# primeiro precisamos agrupar os valores para tornar nossa amostra mais representativa (houve apenas algumas respostas para pessoas com mais de 41 anos)
 dados[dados$Idade == "Entre_41_e_50", 2] = "Mais 41"
 dados[dados$Idade == "Entre_51_e_60", 2] = "Mais 41"
 dados[dados$Idade == "Entre_61_e_70", 2] = "Mais 41"
 dados[dados$Idade == "Mais_de_71", 2] = "Mais 41"
 
-# then we can build a data frame with the variables x and y
+# então podemos construir um data frame com as variáveis x e y
 idade_conceito <- data.frame(prop.table(table(dados$Idade, dados$ConceitoEfluente),1))
 
-# turn into factor for preserves variable and value label attributes
+# transformando em fator para preservar atributos de rótulos de variáveis e valores
 idade_conceito$Var2<-as.factor(idade_conceito$Var2)
 
 idade_conceito$Var2<- factor(idade_conceito$Var2,
@@ -22,14 +22,14 @@ idade_conceito$Var2<- factor(idade_conceito$Var2,
                                         "Tem_ideia",
                                         "Sabe"))
 
-# replace values with new values
-idade_conceito$var2_cat<-revalue(idade_conceito$Var2, replace = c("Nao_sabe" = "Doesn't know",
-                                                                  "Sabe" = "Know",
-                                                                  "Tem_ideia" = "Has some idea about it"))
+# substitua por novos valores
+idade_conceito$var2_cat<-revalue(idade_conceito$Var2, replace = c("Nao_sabe" = "Não sabe",
+                                                                  "Sabe" = "Sabe",
+                                                                  "Tem_ideia" = "Tem ideia"))
 
 idade_conceito$Var2 <-as.numeric(as.character(idade_conceito$Var2))
 
-# turn into factor for preserves variable and value label attributes
+# transformando em fator para preservar atributos de rótulos de variáveis e valores
 idade_conceito$Var1<-as.factor(idade_conceito$Var1)
 
 
@@ -40,7 +40,7 @@ idade_conceito$Var1<- factor(idade_conceito$Var1,
                                         "Entre_31_e_40", 
                                         "Mais 41"))
 
-# replace values with new values
+# substitua por novos valores
 idade_conceito$var1_cat<-revalue(idade_conceito$Var1, replace = c("Entre_15_e_20" = "15-20", 
                                                                   "Entre_21_e_25" = "21-25",
                                                                   "Entre_26_e_30" = "26-30",
@@ -49,9 +49,9 @@ idade_conceito$var1_cat<-revalue(idade_conceito$Var1, replace = c("Entre_15_e_20
 
 idade_conceito$Var1 <-as.numeric(as.character(idade_conceito$Var1))
 
-# building a percentage plot with ggplot2
+# construindo um gráfico percentual com ggplot2
 ggplot(idade_conceito, aes(x = var1_cat, y=Freq, fill =var2_cat ))+
   geom_bar( width = 0.8, position = position_dodge(width = 0.9), stat = "identity")+ 
-  labs(x="Age (years)", y=" ", fill = " ") +
-  ggtitle("Concept on the subject") +
+  labs(x="Idade (anos)", y=" ", fill = " ") +
+  ggtitle("Conceito de efluente") +
   scale_y_continuous(labels = scales::percent)
